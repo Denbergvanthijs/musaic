2c0b7eb1e1e692994521c55584ac297854f18bf0
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-12-12 23:05:12 +0100

    Rough cleanup for readability

--- src/main/python/network.py
+++ src/main/python/network.py
@@ -25,10 +25,10 @@ if PLAYER != RANDOM:
 
 
 class RandomPlayer():
-    ''' For testing purpose only! '''
+    """ For testing purpose only! """
 
     def __init__(self):
-        print('[RandomPlayer]', ' === Using RANDOM PLAYER for testing ===')
+        print("[RandomPlayer] === Using RANDOM PLAYER for testing ===")
 
     def generateBar(self, **kwargs):
         notes = []
@@ -40,59 +40,55 @@ class RandomPlayer():
 
 
 class NeuralNet():
-
     def __init__(self, resources_path=None, init_callbacks=None):
-
-        print('[NeuralNet]', 'Initialising...')
+        print("[NeuralNet]", "Initialising...")
         self.loaded = False
 
         startTime = time.time()
 
         if not resources_path:
-            resources_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../resources/base/')
+            resources_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../resources/base/")
 
         if PLAYER == VER_9:
             trainingsDir = "./src/main/resources/base/v9_lead/"
         elif PLAYER == EUROAI:
             trainingsDir = "./src/main/resources/base/euroAI/"
         else:
-            raise ('[NeuralNet] Unknown player initialised ({}). Aborting'.format(PLAYER))
+            raise (f"[NeuralNet] Unknown player initialised ({PLAYER}). Aborting")
 
-        print('\n[NeuralNet]', ' === Using {} ===\n'.format('VER9' if PLAYER == VER_9 else 'EUROAI'))
+        print(f"[NeuralNet] === Using {'VER9' if PLAYER == VER_9 else 'EUROAI'} ===")
 
-        with open(os.path.join(trainingsDir, 'DataGenerator.conversion_params'), 'rb') as f:
+        with open(os.path.join(trainingsDir, "DataGenerator.conversion_params"), "rb") as f:
             conversionParams = pickle.load(f)
 
-        self.rhythmDict = conversionParams['rhythm']
+        self.rhythmDict = conversionParams["rhythm"]
         for k, v in list(self.rhythmDict.items()):
             self.rhythmDict[v] = k
 
-        self.metaEmbedder = MetaEmbedding.from_saved_custom(os.path.join(trainingsDir, 'meta'))
-        metaPredictor = MetaPredictor.from_saved_custom(os.path.join(trainingsDir, 'meta'))
+        self.metaEmbedder = MetaEmbedding.from_saved_custom(os.path.join(trainingsDir, "meta"))
+        metaPredictor = MetaPredictor.from_saved_custom(os.path.join(trainingsDir, "meta"))
 
-        weightsFolder = os.path.join(trainingsDir, 'weights')
+        weightsFolder = os.path.join(trainingsDir, "weights")
         self.combinedNet = CombinedNetwork.from_saved_custom(weightsFolder, metaPredictor,
                                                              generation=True, compile_now=False)
 
-        self.vocabulary = {
-            'rhythm': self.combinedNet.params['rhythm_net_params'][2],
-            'melody': self.combinedNet.params['melody_net_params'][3]
-        }
+        self.vocabulary = {"rhythm": self.combinedNet.params["rhythm_net_params"][2],
+                           "melody": self.combinedNet.params["melody_net_params"][3]}
 
-        with open(os.path.join(trainingsDir, 'ChordGenerator.conversion_params'), 'rb') as f:
+        with open(os.path.join(trainingsDir, "ChordGenerator.conversion_params"), "rb") as f:
             chordConversionParams = pickle.load(f)
 
-        self.chordDict = chordConversionParams['chords']
+        self.chordDict = chordConversionParams["chords"]
         for k, v in list(self.chordDict.items()):
             self.chordDict[v] = k
 
-        self.chordNet = ChordNetwork.from_saved_custom(os.path.join(trainingsDir, 'chord'),
+        self.chordNet = ChordNetwork.from_saved_custom(os.path.join(trainingsDir, "chord"),
                                                        load_melody_encoder=True)
 
         # Predict some junk data to fully initialise model...
         self.generateBar(**DEFAULT_SECTION_PARAMS, **DEFAULT_AI_PARAMS)
 
-        print('\n[NeuralNet]', 'Neural network loaded in', int(time.time() - startTime), 'seconds\n')
+        print(f"[NeuralNet] Neural network loaded in {int(time.time() - startTime)} seconds")
 
         self.loaded = True
 
@@ -104,24 +100,19 @@ class NeuralNet():
                 init_callbacks()
 
     def generateBar(self, octave=4, **kwargs):
-        ''' Expecting...
-            - 'lead_bar'
-            - 'prev_bars'
-            - 'sample_mode'
-            - 'chord_mode'
-            - 'lead_mode'
-            - 'context_mode'
-            - 'injection_params'
-            - 'meta_data'
-            - 'octave'
-        '''
-
-        #print('[NeuralNet]', 'generateBar with params')
-        # for k, v in kwargs.items():
-        #    print('  ', k, v)
-
+        """ Expecting...
+            - "lead_bar"
+            - "prev_bars"
+            - "sample_mode"
+            - "chord_mode"
+            - "lead_mode"
+            - "context_mode"
+            - "injection_params"
+            - "meta_data"
+            - "octave"
+        """
         rhythmContexts, melodyContexts = self.getContexts(kwargs)
-        embeddedMetaData = self.embedMetaData(kwargs['meta_data'])
+        embeddedMetaData = self.embedMetaData(kwargs["meta_data"])
         leadRhythm, leadMelody = self.getLead(kwargs, rhythmContexts, melodyContexts)
 
         output = self.combinedNet.predict(x=[*rhythmContexts,
@@ -142,10 +133,9 @@ class NeuralNet():
         if not metaData:
             metaData = DEFAULT_META_DATA
         values = []
-        #print('[NeuralNet]', 'embedMetaData:')
+
         for k in sorted(metaData.keys()):
-            #print(k, metaData[k])
-            if k == 'ts':
+            if k == "ts":
                 values.extend([4, 4])
             else:
                 values.append(metaData[k])
@@ -155,35 +145,30 @@ class NeuralNet():
         return self.metaEmbedder.predict(md)
 
     def getContexts(self, kwargs):
-        mode = kwargs.get('context_mode', None)
-        injection_params = kwargs.get('injection_params',
-                                      DEFAULT_AI_PARAMS['injection_params'])
-        prev_bars = kwargs.get('prev_bars')
+        mode = kwargs.get("context_mode", None)
+        injection_params = kwargs.get("injection_params", DEFAULT_AI_PARAMS["injection_params"])
+        prev_bars = kwargs.get("prev_bars")
 
-        if mode == 'inject':
+        if mode == "inject":
             rhythmPool = []
             for rhythmType in injection_params[0]:
                 # *2 gives extra weight to non-empty beats
-                rhythmPool.extend({
-                    'qb': [self.rhythmDict[(0.0,)]] * 2,
-                    'lb': [self.rhythmDict[()]],
-                    'eb': [self.rhythmDict[(0.0, 0.5)],
-                           self.rhythmDict[(0.5,)]] * 2,
-                    'fb': [self.rhythmDict[(0.0, 0.25, 0.5, 0.75)],
-                           self.rhythmDict[(0.0, 0.25, 0.5)],
-                           self.rhythmDict[(0.5, 0.75)]] * 2,
-                    'tb': [self.rhythmDict[(0.0, 0.333, 0.6667)],
-                           self.rhythmDict[(0.3333, 0.6667)]] * 2,
-                }[rhythmType])
+                rhythmPool.extend({"qb": [self.rhythmDict[(0.0,)]] * 2,
+                                   "lb": [self.rhythmDict[()]],
+                                   "eb": [self.rhythmDict[(0.0, 0.5)], self.rhythmDict[(0.5,)]] * 2,
+                                   "fb": [self.rhythmDict[(0.0, 0.25, 0.5, 0.75)],
+                                          self.rhythmDict[(0.0, 0.25, 0.5)],
+                                          self.rhythmDict[(0.5, 0.75)]] * 2,
+                                   "tb": [self.rhythmDict[(0.0, 0.333, 0.6667)],
+                                          self.rhythmDict[(0.3333, 0.6667)]] * 2, }[rhythmType])
 
             rhythmContexts = [np.random.choice(rhythmPool, size=(1, 4)) for _ in range(4)]
 
-            melodyPool = {
-                'maj': [1, 3, 5, 6, 8, 10, 12],
-                'min': [1, 3, 4, 6, 8, 10, 11],
-                'pen': [1, 4, 6, 8, 11],
-                '5th': [1, 8]
-            }[injection_params[1]]
+            melodyPool = {"maj": [1, 3, 5, 6, 8, 10, 12],
+                          "min": [1, 3, 4, 6, 8, 10, 11],
+                          "pen": [1, 4, 6, 8, 11],
+                          "5th": [1, 8]}[injection_params[1]]
+
             # if len(injection_params) > 2 and injection_params[2]:
             melodyPool.extend([x + 12 for x in melodyPool])
 
@@ -192,50 +177,52 @@ class NeuralNet():
         else:
             rhythmContexts = np.zeros((4, 1, 4))
             melodyContexts = np.zeros((1, 4, 48))
+
             for i, b in enumerate(prev_bars[-4:]):
                 r, m = self.convertBarToContext(b)
                 rhythmContexts[i, :, :] = r
                 melodyContexts[:, i, :] = m
 
-        # print('[NeuralNet]', 'Contexts:', rhythmContexts, melodyContexts)
+        # print("[NeuralNet]", "Contexts:", rhythmContexts, melodyContexts)
 
         return rhythmContexts, melodyContexts
 
     def getLead(self, kwargs, rhythmContexts, melodyContexts):
-        if 'lead_mode' not in kwargs or not kwargs['lead_mode']:
+        if "lead_mode" not in kwargs or not kwargs["lead_mode"]:
             leadRhythm = rhythmContexts[-1]
             leadMelody = melodyContexts[:, -1:, :]
-        elif kwargs['lead_mode'] == 'both':
-            leadRhythm, leadMelody = self.convertBarToContext(kwargs['lead_bar'])
-        elif kwargs['lead_mode'] == 'melody':
+
+        elif kwargs["lead_mode"] == "both":
+            leadRhythm, leadMelody = self.convertBarToContext(kwargs["lead_bar"])
+
+        elif kwargs["lead_mode"] == "melody":
             leadRhythm = rhythmContexts[-1]
-            _, leadMelody = self.convertBarToContext(kwargs['lead_bar'])
+            _, leadMelody = self.convertBarToContext(kwargs["lead_bar"])
 
         return leadRhythm, leadMelody
 
     def sampleOutput(self, output, kwargs):
-        mode = kwargs.get('sample_mode', 'dist')
-        chord_mode = kwargs.get('chord_mode', 1)
-        if chord_mode in {'force', 'auto'}:
+        mode = kwargs.get("sample_mode", "dist")
+        chord_mode = kwargs.get("chord_mode", 1)
+
+        if chord_mode in ("force", "auto"):
             chord_num = 1
         else:
             chord_num = int(chord_mode)
 
-        # print('[NeuralNet]', 'sampleOutput', 'chord_mode', chord_mode, 'chord_num', chord_num)
+        # print("[NeuralNet]", "sampleOutput", "chord_mode", chord_mode, "chord_num", chord_num)
 
-        if mode == 'argmax' or mode == 'best':
+        if mode in ("argmax", "best"):
             sampledRhythm = np.argmax(output[0], axis=-1)
             sampledMelody = np.argmax(output[1], axis=-1)
-            sampledChords = [list(rand.choice(self.vocabulary['melody'], p=curr_p,
+            sampledChords = [list(rand.choice(self.vocabulary["melody"], p=curr_p,
                                               size=chord_num, replace=True)) for curr_p in output[1][0]]
-        elif mode == 'dist':
-            sampledRhythm = np.array([[np.random.choice(self.vocabulary['rhythm'], p=dist)
-                                       for dist in output[0][0]]])
-            sampledMelody = np.array([[np.random.choice(self.vocabulary['melody'], p=dist)
-                                       for dist in output[1][0]]])
-            sampledChords = [list(rand.choice(self.vocabulary['melody'], p=curr_p, size=chord_num,
-                                              replace=True)) for curr_p in output[1][0]]
-        elif mode == 'top':
+        elif mode == "dist":
+            sampledRhythm = np.array([[np.random.choice(self.vocabulary["rhythm"], p=dist) for dist in output[0][0]]])
+            sampledMelody = np.array([[np.random.choice(self.vocabulary["melody"], p=dist) for dist in output[1][0]]])
+            sampledChords = [list(rand.choice(self.vocabulary["melody"], p=curr_p, size=chord_num, replace=True))
+                             for curr_p in output[1][0]]
+        elif mode == "top":
             # Random from top 5 predictions....
             r = []
             sampledChords = []
@@ -260,17 +247,16 @@ class NeuralNet():
                                                       replace=True, size=chord_num)))
             sampledMelody = np.array([m])
 
-        # print('[NeuralNet]', sampledRhythm.shape, sampledMelody.shape)
+        # print("[NeuralNet]", sampledRhythm.shape, sampledMelody.shape)
         return sampledRhythm, sampledMelody, sampledChords
 
     def convertBarToContext(self, measure):
-        '''
+        """
         Converts a list of notes (nn, start_tick, end_tick) to context
         format for network to use
-        '''
-
+        """
         if not measure or measure.isEmpty():
-            # empty bar...
+            # Empty bar...
             rhythm = [self.rhythmDict[()] for _ in range(4)]
             melody = [random.choice([1, 7]) for _ in range(48)]
             return np.array([rhythm]), np.array([[melody]])
@@ -301,7 +287,7 @@ class NeuralNet():
             try:
                 rhythm.append(self.rhythmDict[tuple(word)])
             except KeyError:
-                print('[NeuralNet]', 'Beat not found, using eigth note...')
+                print("[NeuralNet] Beat not found, using eigth note...")
                 rhythm.append(self.rhythmDict[(0.0, 0.5)])
 
         if len(pcs) == 0:
@@ -321,19 +307,18 @@ class NeuralNet():
             note = (int(nn), startTick, endTick)
             return note
 
-        def predictChord(notes, pc, sample_mode, melodyContext, metaData, chord_mode='auto'):
+        def predictChord(notes, pc, sample_mode, melodyContext, metaData, chord_mode="auto"):
             values = []
             for k in sorted(metaData.keys()):
-                if k == 'ts':
+                if k == "ts":
                     values.extend([4, 4])
                 else:
                     values.append(metaData[k])
             md = np.tile(values, (1, 1))
 
-            chord_outputs = self.chordNet.predict(
-                x=[np.array([[pc]]), np.array([[melodyContext]]), md]
-            )
-            if sample_mode == 'dist' or sample_mode == 'top':
+            chord_outputs = self.chordNet.predict(x=[np.array([[pc]]), np.array([[melodyContext]]), md])
+
+            if sample_mode == "dist" or sample_mode == "top":
                 chord = rand.choice(len(chord_outputs[0]), p=chord_outputs[0])
             else:
                 chord = np.argmax(chord_outputs[0], axis=-1)
@@ -346,18 +331,18 @@ class NeuralNet():
 
             return notes
 
-        if 'meta_data' not in kwargs or kwargs['meta_data'] == None:
-            kwargs['meta_data'] = deepcopy(DEFAULT_META_DATA)
+        if "meta_data" not in kwargs or kwargs["meta_data"] == None:
+            kwargs["meta_data"] = deepcopy(DEFAULT_META_DATA)
 
         notes = []
         onTicks = [False] * 96
 
-        chord_mode = kwargs.get('chord_mode', 1)
-        if chord_mode not in {'force', 'auto'}:
+        chord_mode = kwargs.get("chord_mode", 1)
+        if chord_mode not in {"force", "auto"}:
             chord_mode = int(chord_mode)
 
-        # print('[NeuralNet]', 'convertContextToNotes', 'chord_mode', chord_mode)
-        sample_mode = kwargs.get('sample_mode', 'top')
+        # print("[NeuralNet]", "convertContextToNotes", "chord_mode", chord_mode)
+        sample_mode = kwargs.get("sample_mode", "top")
 
         for i, beat in enumerate(rhythmContext):
             b = self.rhythmDict[beat]
@@ -373,13 +358,13 @@ class NeuralNet():
                 endTick = 96
             pc = melodyContext[i // 2]
 
-            if chord_mode == 'force':
+            if chord_mode == "force":
                 tonic = 12 + (pc % 12)
-                notes = predictChord(notes, tonic, sample_mode, melodyContext, kwargs['meta_data'])
-            elif chord_mode in (0, 1, 'auto'):
+                notes = predictChord(notes, tonic, sample_mode, melodyContext, kwargs["meta_data"])
+            elif chord_mode in (0, 1, "auto"):
                 if pc >= 12:
-                    # draw chord intervals...
-                    notes = predictChord(notes, pc, sample_mode, melodyContext, kwargs['meta_data'], chord_mode=chord_mode)
+                    # Draw chord intervals...
+                    notes = predictChord(notes, pc, sample_mode, melodyContext, kwargs["meta_data"], chord_mode=chord_mode)
                 else:
                     notes.append(makeNote(pc, tick, endTick))
 
@@ -511,9 +496,9 @@ class TransformerNet(NeuralNet):
         """Convert a context to a list of notes.
 
         Changes compared to NeuralNet:
-        - Provide the chord_mode to predictChord when the chord_mode is 'force'
+        - Provide the chord_mode to predictChord when the chord_mode is "force"
         - Moved functions outside convertContextToNotes
-        - Removed the try except block and replaced with if-else to check if we're at the end of the list
+        - Removed the try except block and replaced with if-else to check if we"re at the end of the list
         """
         if "meta_data" not in kwargs or kwargs["meta_data"] is None:
             kwargs["meta_data"] = deepcopy(DEFAULT_META_DATA)
@@ -562,7 +547,6 @@ class TransformerNet(NeuralNet):
 
 
 class NetworkEngine(multiprocessing.Process):
-
     def __init__(self, requestQueue, returnQueue, resources_path=None, init_callbacks=None):
         super(NetworkEngine, self).__init__()
 
@@ -596,18 +580,18 @@ class NetworkEngine(multiprocessing.Process):
         while not self.stopRequest.is_set():
             try:
                 requestMsg = self.requestQueue.get(timeout=1)
-                # print('[NetworkEngine]', 'request recieved from', requestMsg['measure_address'])
+                # print("[NetworkEngine]", "request received from", requestMsg["measure_address"])
             except multiprocessing.queues.Empty:
-                # print('no messages recieved yet')
+                # print("No messages received yet")
                 continue
 
-            # print('generating result...')
+            # print("Generating result...")
             result = self.network.generateBar(**requestMsg["request"])
-            # print('generated result')
+            # print("Generated result")
 
             self.returnQueue.put({"measure_address": requestMsg["measure_address"], "result": result})
 
-            time.sleep(0.01)
+            time.sleep(0.01)  # TODO: try to remove this
 
     def isLoaded(self) -> bool:
         """Returns True if the network is loaded, False otherwise."""
