676485aa113b4f01f46491a56c2a078c151f2455
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-12-31 17:09:54 +0100

    Removed old code, now uses F1

--- src/main/python/train_simple_model.py
+++ src/main/python/train_simple_model.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 import numpy as np
 import tensorflow as tf
-from smt22.models import build_model, build_simple_model, build_simpler_model
-from smt22.utils import plots, preprocess, valid_input
+from matplotlib import pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, f1_score
+from smt22.models import build_simpler_model
+from smt22.utils import f1_custom, preprocess, valid_input
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
-from tensorflow.keras.utils import plot_model
 from v9.Data.DataGeneratorsTransformer import CombinedGenerator
 
 if __name__ == "__main__":
-    USE_SIMPLE_MODEL = True
     # Inputs
     fp_music = "./src/main/python/v9/Data/lessfiles"  # "../../Data/music21"
     fp_output = "./src/main/python/smt22/"  # "../../Data/music21
@@ -32,9 +32,8 @@ if __name__ == "__main__":
     data_iter = combined_generator.generate_data(rhythm_context_size=4, melody_context_size=4)
 
     cnt = 0
-    ys_rhythm, ys_melody = [], []
-    X1, X2, X3, X4, X5 = [], [], [], [], []
     Xs = []
+    ys_rhythm, ys_melody = [], []
     for c, (X, y) in enumerate(data_iter):
         X, y_rhythm, y_melody = preprocess(X, y)  # Preprocess a single track
 
@@ -44,16 +43,8 @@ if __name__ == "__main__":
 
         context_rhythms, context_melodies, meta, lead_rhythm, lead_melody = X
         for row in range(context_rhythms.shape[0]):
-            X1.append(context_rhythms[row])
-            X2.append(context_melodies[row])
-            X3.append(meta[row])
-            X4.append(lead_rhythm[row])
-            X5.append(lead_melody[row])
-
-            # Flatten all in X0
-            if USE_SIMPLE_MODEL:
-                Xs.append(np.concatenate((context_rhythms[row], context_melodies[row],
-                          meta[row], lead_rhythm[row], lead_melody[row]), axis=0))
+            Xs.append(np.concatenate((context_rhythms[row], context_melodies[row],
+                                      meta[row], lead_rhythm[row], lead_melody[row]), axis=0))
 
             ys_rhythm.append(y_rhythm[row])
             ys_melody.append(y_melody[row])
@@ -61,12 +52,11 @@ if __name__ == "__main__":
         # if c == 4:  # Early stop for testing
         #     break
 
-    if USE_SIMPLE_MODEL:
-        Xs = np.array(Xs)
-    else:
-        Xs = [np.array(X1), np.array(X2), np.array(X3), np.array(X4), np.array(X5)]
+    Xs = np.array(Xs)
 
-    ys = [np.array(ys_rhythm), np.array(ys_melody)]
+    ys_rhythm = np.array(ys_rhythm)
+    ys_melody = np.array(ys_melody)
+    ys = [ys_rhythm, ys_melody]
 
     print(f"Xs: {len(Xs)} tracks; {Xs[0].shape} {Xs[1].shape} {Xs[2].shape} {Xs[3].shape} {Xs[4].shape}")
     print(f"Max values: {np.max(Xs[0])} {np.max(Xs[1])} {np.max(Xs[2])} {np.max(Xs[3])} {np.max(Xs[4])}")
@@ -77,11 +67,6 @@ if __name__ == "__main__":
     print(f"Skipped {cnt} ({cnt/sum(num_pieces)*100:.0f}%) tracks because of wrong shape")
 
     # Shape is (batch_size, n_repeats, output_shape)
-    if USE_SIMPLE_MODEL:
-        model = build_simple_model(output_length_rhythm=4, n_repeat_rhythm=127, output_length_melody=48, n_repeat_melody=25)
-    else:
-        model = build_model(output_length_rhythm=4, n_repeat_rhythm=127, output_length_melody=48, n_repeat_melody=25)
-
     fp_logs = os.path.join("./src/main/python/smt22/logs", datetime.now().strftime("%Y%m%d_%H%M%S"))
     tensorboard_cb = TensorBoard(log_dir=fp_logs, histogram_freq=1)
 
@@ -94,8 +79,11 @@ if __name__ == "__main__":
     # lr_schedule = PolynomialDecay(initial_learning_rate=0.01, decay_steps=1_000, end_learning_rate=0.0005)
     opt_rhythm = Adam(learning_rate=0.1, beta_1=0.9, beta_2=0.999, clipnorm=None)
     opt_melody = Adam(learning_rate=0.05, beta_1=0.9, beta_2=0.999, clipnorm=None)
-    model_rhythm.compile(optimizer=opt_rhythm, loss="binary_crossentropy", metrics="categorical_accuracy")
-    model_melody.compile(optimizer=opt_melody, loss="binary_crossentropy", metrics="categorical_accuracy")
+
+    precision = tf.keras.metrics.Precision(name='precision')
+    recall = tf.keras.metrics.Recall(name='recall')
+    model_rhythm.compile(optimizer=opt_rhythm, loss="categorical_crossentropy", metrics=[f1_custom, precision, recall])
+    model_melody.compile(optimizer=opt_melody, loss="categorical_crossentropy", metrics=[f1_custom, precision, recall])
 
     model_rhythm.summary()
     model_melody.summary()
@@ -110,13 +98,27 @@ if __name__ == "__main__":
     model_rhythm.save("./src/main/python/smt22/model_original_rhythm.h5")
     model_melody.save("./src/main/python/smt22/model_original_melody.h5")
 
-    # converter = tf.lite.TFLiteConverter.from_keras_model(model)
-    # converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    # tflite_model = converter.convert()
+    y_pred_rhythm = model_rhythm.predict(Xs)
+    y_pred_rhythm = (y_pred_rhythm > 0.5).astype(int).flatten()
 
-    # # Save the model
-    # with open("./src/main/python/smt22/model.tflite", "wb") as file:
-    #     file.write(tflite_model)
+    y_pred_melody = model_melody.predict(Xs)
+    y_pred_melody = (y_pred_melody > 0.5).astype(int).flatten()
+
+    # Calculate F1 score
+    print(f"F1 Rhythm: {f1_score(ys_rhythm.flatten(), y_pred_rhythm, average='macro')}")
+    print(f"F1 Melody: {f1_score(ys_melody.flatten(), y_pred_melody, average='macro')}")
+
+    cm_rhythm = confusion_matrix(np.array(ys_rhythm).flatten(), y_pred_rhythm)
+    print(cm_rhythm)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm_rhythm)
+    disp.plot()
+    plt.show()
+
+    cm_melody = confusion_matrix(np.array(ys_melody).flatten(), y_pred_melody)
+    print(cm_melody)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm_melody)
+    disp.plot()
+    plt.show()
 
     score_rhythm = model_rhythm.evaluate(Xs, np.array(ys_rhythm), verbose=1, batch_size=32, use_multiprocessing=True, workers=6)
     score_melody = model_melody.evaluate(Xs, np.array(ys_melody), verbose=1, batch_size=32, use_multiprocessing=True, workers=6)
