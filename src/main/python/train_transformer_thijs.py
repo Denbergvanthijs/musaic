0f5b84e1e18d9fe30e2b5a25cf5a7edd41d38a94
Denbergvanthijs <denbergvanthijs@gmail.com>
2022-12-31 17:02:30 +0100

    Added f1 plots

--- src/main/python/train_transformer_thijs.py
+++ src/main/python/train_transformer_thijs.py
@@ -4,8 +4,10 @@ from datetime import datetime
 
 import numpy as np
 import tensorflow as tf
+from matplotlib import pyplot as plt
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix, f1_score
 from smt22.models import build_model, build_simple_model
-from smt22.utils import plots, preprocess, valid_input
+from smt22.utils import f1_custom, plots, preprocess, valid_input
 from tensorflow.keras.callbacks import TensorBoard
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.optimizers.schedules import PolynomialDecay
@@ -66,7 +68,9 @@ if __name__ == "__main__":
     else:
         Xs = [np.array(X1), np.array(X2), np.array(X3), np.array(X4), np.array(X5)]
 
-    ys = [np.array(ys_rhythm), np.array(ys_melody)]
+    ys_rhythm = np.array(ys_rhythm)
+    ys_melody = np.array(ys_melody)
+    ys = [ys_rhythm, ys_melody]
 
     print(f"Xs: {len(Xs)} tracks; {Xs[0].shape} {Xs[1].shape} {Xs[2].shape} {Xs[3].shape} {Xs[4].shape}")
     print(f"Max values: {np.max(Xs[0])} {np.max(Xs[1])} {np.max(Xs[2])} {np.max(Xs[3])} {np.max(Xs[4])}")
@@ -87,13 +91,36 @@ if __name__ == "__main__":
 
     # lr_schedule = PolynomialDecay(initial_learning_rate=0.001, decay_steps=1_000, end_learning_rate=0.0005)
     opt = Adam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, clipnorm=None)
-    model.compile(optimizer=opt, loss="binary_crossentropy", metrics=["accuracy"], loss_weights=[1, 1])
+    precision = tf.keras.metrics.Precision(name='precision')
+    recall = tf.keras.metrics.Recall(name='recall')
+    model.compile(optimizer=opt, loss="categorical_crossentropy", metrics=[f1_custom, precision, recall])
 
     model.summary()
     plot_model(model, to_file="./src/main/python/smt22/model_thijs.png", show_shapes=True, dpi=300)
 
     hist_model = model.fit(Xs, ys, epochs=10, verbose=1, batch_size=32, validation_split=0.15,
                            shuffle=True, use_multiprocessing=True, workers=6, callbacks=[tensorboard_cb])
+
+    # Plot confusion matrix
+    y_pred = model.predict(Xs)
+    y_pred_rhythm = (y_pred[0] > 0.5).astype(int).flatten()
+    y_pred_melody = (y_pred[1] > 0.5).astype(int).flatten()
+
+    # Calculate F1 scores
+    print(f"F1 Rhythm: {f1_score(ys_rhythm.flatten(), y_pred_rhythm, average='macro')}")
+    print(f"F1 Melody: {f1_score(ys_melody.flatten(), y_pred_melody, average='macro')}")
+
+    cm_rhythm = confusion_matrix(np.array(ys_rhythm).flatten(), y_pred_rhythm)
+    print(cm_rhythm)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm_rhythm)
+    disp.plot()
+    plt.show()
+
+    cm_melody = confusion_matrix(np.array(ys_melody).flatten(), y_pred_melody)
+    print(cm_melody)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm_melody)
+    disp.plot()
+    plt.show()
 
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
@@ -107,4 +134,4 @@ if __name__ == "__main__":
     print(f"Rhythm model loss: {score[1]:.4f}; Melody model loss: {score[2]:.4f}")
     print(f"Rhythm model accuracy: {score[3]:.4f}; Melody model accuracy: {score[4]:.4f}")
 
-    plots(hist_model)
+    plots(hist_model, "f1_custom")
